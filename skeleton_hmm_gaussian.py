712e5e28c42979dfa591d6a10ee686fbd2466a84
gli123456 <43455032+gli123456@users.noreply.github.com>
2019-04-09 11:22:18 -0400

    Update skeleton_hmm_gaussian.py

--- skeleton_hmm_gaussian.py
+++ skeleton_hmm_gaussian.py
@@ -4,8 +4,7 @@ if not __file__.endswith('_hmm_gaussian.py'):
     print('ERROR: This file is not named correctly! Please name it as Lastname_hmm_gaussian.py (replacing Lastname with your last name)!')
     exit(1)
 
-DATA_PATH = "../data"
-#DATA_PATH = "/u/cs246/data/em/" #TODO: if doing development somewhere other than the cycle server (not recommended), then change this to the directory where your data file is (points.dat)
+DATA_PATH = "../data" #TODO: if doing development somewhere other than the cycle server (not recommended), then change this to the directory where your data file is (points.dat)
 
 def parse_data(args):
     num = float
